e86423703d660cd6f94e7c0444f68e1c8b14e77e
rlarlghd1004 <rlarlghd1004>
2016-02-21 22:55:32 -0800

    a

--- hello.c
+++ hello.c
@@ -2,14 +2,6 @@
 
 int main()
 {
-<<<<<<< HEAD
-	printf("hello world");
-	printf("kihong did it!");
-	printf("kihong did it! version 2");
-=======
-	printf("hello world\n");
-	printf("kihong did it!\n");
-	printf("Also Jeehun did it!\n");
->>>>>>> 275e6a1164e0f6e89e84fc308b52b456d06391ad
+
 	return 0;
 }
